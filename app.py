dd8bcb1fbb068983138cdfa685b6c3ebde1ef000
Cruz <Onel.Cruz@G3TI.Local>
2023-11-10 14:55:31 -0500

    Adding and modifying web files.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import redirect
+from flask import request
 from flask import url_for
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -18,5 +18,6 @@ def gallery():
 def contact():
     return render_template("contact-us.html")
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
